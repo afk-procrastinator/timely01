13ecfefb479e6a304db851ecaaff2d90dc8a3f6c
Spencer <spencermichaels1@gmail.com>
2020-06-15 11:48:07 +0300

    REMINDER LIVES

--- listener/reminder.py
+++ listener/reminder.py
@@ -23,7 +23,7 @@ gmaps = googlemaps.Client(key=settings.GMAPS)
 token = settings.TOKEN
 bot = commands.Bot(command_prefix=commandKey)
 botColor = 0x176BD3
-
+userSend = discord.Member
 
 month = ["months", "month", "mo", "mos"]
 week = ["weeks", "week", "wk", "wks"]
@@ -32,7 +32,7 @@ hour = ["hours", "hour", "hr", "hrs"]
 min = ["minutes", "minute", "min", "mins"]
 csv_columns = ["timeAdd", "user", "timeCreated", "message", "guild"]
 loopDict = {}
-sendDict = {} # NOT IN USE RN 
+addDict = {}
 
 resetLoop = False
 
@@ -51,7 +51,7 @@ async def sendDM(input):
     await channel.send(embed = embed)
 
 async def append(length: str, input: dict):
-    toOpen = data_folder / length
+    toOpen = length
     if os.path.isfile(toOpen):
         print ("File exists and is readable")   
         try:
@@ -70,6 +70,7 @@ async def append(length: str, input: dict):
         except IOError:
             print("error")
 
+
 async def addToLoop(self, input: dict):
     global loopDict
     global resetLoop
@@ -77,65 +78,35 @@ async def addToLoop(self, input: dict):
     print(length)
     if length == 0:
         task = asyncio.create_task(runLoop())
-        taskShort = asyncio.create_task(runShort(self))
         resetLoop = False
         loopDict[str(length)] = (input)
     else: 
         resetLoop = True
         loopDict[str(length)] = (input)
         resetLoop = False
-
-# Takes dict and figures out where it belongs 
-async def runAppend(self, importDict: dict):
-    timeAdd = importDict["timeAdd"]
-    guild = importDict["guild"]
-    guildStringLong = "long.csv"
-    guildStringShort = "short.csv"
-    if timeAdd > 60 and timeAdd < 1440 : # over a hour from now but less than an 24 hours -- short
-        await append(guildStringShort, importDict)
-    elif timeAdd >= 1440: # over a day from now
-        await append(guildStringLong, importDict)
-    elif timeAdd <= 61: # less than an hour than now -- loop dict THIS IS WHERE WORK IS NEEDED 
-        await addToLoop(self, importDict)
-
-async def runShort(self):
-    while True:
-        print("repeat")
-        with open('files/short.csv', newline='', mode = 'r') as csvfile:
-            dict_list = []
-            reader = csv.DictReader(csvfile)
-            for line in reader:
-                line["timeAdd"] = int(line["timeAdd"]) - 1
-                print(line)
-                #dict_list[]
-        
-        os.remove("files/short.csv")
-        with open('files/short.csv', newline='', mode = 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames = csv_columns)
-            writer.writeheader()
-            writer.writerows(dict_list)
-        
-        await asyncio.sleep(1)
- 
     
 async def runLoop():
     global resetLoop
     global loopDict
+    global addDict
     while True:
         if resetLoop == False:
-            print("running")
+            #print("running")
             print(loopDict)
-            for key in loopDict:
-                print(loopDict[key]["timeAdd"])
+            for key in list(loopDict):
+                #print(loopDict[key]["timeAdd"])
                 loopDict[key]["timeAdd"] -= 1
                 if loopDict[key]["timeAdd"] == 0:
                     await sendDM(loopDict[key])
                     del loopDict[key]
-                print(loopDict[key]["timeAdd"])
+                    print("deleted")
+                    print(loopDict)
+                print(loopDict)
+                #print(loopDict[key]["timeAdd"])
         elif resetLoop == True:
             print("stopping")
             break
-        await asyncio.sleep(1)
+        await asyncio.sleep(60)
 
 # Formats various lengths to minutes
 def formatToMin(self, inFormat, difference):
@@ -149,19 +120,13 @@ def formatToMin(self, inFormat, difference):
             return difference * 10080
         elif inFormat in month:
             return difference * 43800
+    
 
 class ReminderListener(commands.Cog):
-
     @bot.command()
-    async def test(self, ctx):
-        await runShort(self)
-
-    @bot.command()
-    async def remind(self, ctx, user: discord.Member, message: str, *arg):
-        global ctxG
+    async def remind(self, ctx, user: discord.Member, message: str, *arg,):
         global userSend
         userSend = user
-        ctxG = ctx
         command = []
         for args in arg: 
             command.append(args)
@@ -179,19 +144,19 @@ class ReminderListener(commands.Cog):
             timeAdd += additional
             i += 2
         dictToSend = dict({"timeAdd": timeAdd, "user": user.id, "timeCreated": timeCreated, "message": message, "guild": ctx.guild.id})
-        await runAppend(self, dictToSend)
+        await addToLoop(self, dictToSend)
         embed = discord.Embed(title="⏲ Your reminder is set! ⏲", colour=discord.Colour(botColor))
         embed.add_field(name = "You will be reminded in:", value = str(timeAdd) + " minutes")
         embed.add_field(name = "Your message is:", value = message)
         await ctx.send(embed = embed)
-
+'''
     @remind.error
     async def remind_error(self, ctx, error):
         if isinstance(error, commands.BadArgument) or isinstance(error, commands.MissingRequiredArgument):
             embed = discord.Embed(title="Syntax error >:(", colour = discord.Colour(botColor))
             embed.add_field(name = "Correct syntax:", value = "`?remind @user \"message\" time`\ne.g. `?remind @TimeVibe \"Your message here\" 1 week 10 hours 3 minutes`")
             await ctx.send(embed = embed)
-
+'''
 
 '''
 Every minute run a function that loops through loopDict
